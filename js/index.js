8168afb448b161dc9ad1947b94b1a6f038f00f48
Jerovsky Slava <mako11.83@mail.ru>
2022-01-08 21:20:59 +0300

    did my project

--- js/index.js
+++ js/index.js
@@ -7,7 +7,47 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $('.sale-slider').slick({
+      slidesToShow: 5,
       arrows: false,
-      dots: true
+      dots: true,
+      Infinity: true,
+      autoplay:false,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow:4, 
+          }
+        },
+
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow:3, 
+          }
+        },
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow:1, 
+            
+          }
+        },
+      ]
+    });
+  });
+
+  $(document).ready(function(){
+    $('.title-slider').slick({
+      arrows: false,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 426,
+          settings: {
+            dots: false, 
+          }
+        },
+      ]
     });
   });
